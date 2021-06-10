503d576cdb379d0a1f6f92afa2ed4108288b0dee
Allan-Fry <allan.fry.it@gmail.com>
2021-06-10 19:05:17 +1200

    Added Linkdin and github link

--- javascript-animation/src/Componenrs/Footer/Footer.jsx
+++ javascript-animation/src/Componenrs/Footer/Footer.jsx
@@ -5,9 +5,9 @@ export const Footer = () =>{
 
 return(
     <div className="container">
-        <a className="footLink" href="#">Linkdin</a>
+        <a className="footLink" href="https://www.linkedin.com/in/allan-fry/">Linkdin</a>
         <a className="footLink" href="#">Email</a>
-        <a className="footLink" href="#">GitHub</a>
+        <a className="footLink" href="https://github.com/Allan-Fry">GitHub</a>
         <a className="footLink" href="#">Profile</a>
     </div>
 );
